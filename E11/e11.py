92fc51d7b7490be1936d3ec195002ff339adfacf
ali-04 <mahmohamad560@gmail.com>
2021-04-03 00:24:15 -0700

    e11

---
+++ E11/e11.py
@@ -0,0 +1,103 @@
+from selenium import webdriver as wd
+from json import loads
+from os import getcwd
+global _students
+_students = {}
+
+
+with open(getcwd()+'setting.json' ,'r') as F:
+    setting = loads(F.read())
+
+
+ostad = input("ostad?  ")
+
+global url
+url = 'https://www.skyroom.online/ch/mr_sedaghat/ostad-%s'%ostad
+
+
+
+class stu :
+    _students = {}
+    def __init__ (self,name,password):
+        self.name = name
+        self.password = password
+        stu._students[len(stu._students)] = self
+        
+
+
+    def __str__ (self):
+        return self.name
+
+    def print ():
+        l = []
+        for q0 in stu._students:
+            q = stu._students[q0]
+            l.append(str(q0)+"_"+str(q))
+        return("\n".join(l))
+
+    def _apend (self):
+        self.driver = wd.Firefox()
+        self.driver.get(url)
+        self.driver.find_element_by_xpath('//*[@id="username"]').send_keys('stu_' + self.name)
+        self.driver.find_element_by_xpath('//*[@id="password"]').send_keys(self.password)
+        self.driver.find_element_by_xpath('//*[@id="btn_login"]').click()
+
+
+    def delete (self):
+        try: del self.driver
+        except: pass
+        del self
+
+    def delete_all():
+     #  print(stu._students)
+        for q0 in stu._students:
+            q = stu._students[q0]
+            q.delete()
+        stu._students = {}
+    
+
+    def dast(self):
+        self.driver.find_element_by_xpath('//*[@id="toolbar"]/button[7]').click()
+
+
+
+
+
+
+for q in setting['students']: stu(q,setting['students'][q])
+
+def loading(m=None):
+
+    
+    stu.delete_all()
+    
+
+    with open(getcwd()+'setting.json' ,'r') as F:
+        setting = loads(F.read())
+
+    for q in setting['students']: stu(q,setting['students'][q])
+
+print (stu.print())
+
+
+
+
+
+
+
+
+_code = {
+    'append' : lambda x : stu._students[int(x)]._apend()   ,
+    'reload' : lambda q : loading() ,
+    'print'  : lambda x : print(stu.print()) ,
+    'dast'   : lambda x : stu._students[int(x)].dast()
+}
+
+while True:
+    code = input('code>> ').split()
+    try:en = ' '.join(code[1:])
+    except:en = None
+    try:_code[code[0]] (en)
+    except:pass
+
+
